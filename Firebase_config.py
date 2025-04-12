06e30361116b9a2cfd4c63fd1c45e85f29350679
lab87 <a@gmail.com>
2025-04-12 06:58:43 +0530

    resume upload

---
+++ Firebase_config.py
@@ -0,0 +1,9 @@
+from firebase_admin import credentials, storage, initialize_app
+
+cred = credentials.Certificate('FirebaseKey.json')
+
+firebase_app = initialize_app(cred)
+
+storage_bucket = storage.bucket('hackathons-5f2de.firebasestorage.app')
+
+print(storage_bucket)
