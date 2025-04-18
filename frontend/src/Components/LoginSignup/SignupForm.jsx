457d0af16fcab6aecc55187a0c4c10ceb02faa88
Aryan <aryanrajput039@gmail.com>
2025-04-18 16:33:36 +0530

    fixed login/signup

--- frontend/src/Components/LoginSignup/SignupForm.jsx
+++ frontend/src/Components/LoginSignup/SignupForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import './LoginSignupform.css';
 
-const SignupForm = () => {
+const SignupForm = ({showLogin, setShowLogin}) => {
   const [formData, setFormData] = useState({
     name: '',
     number: '',
@@ -26,7 +26,11 @@ const SignupForm = () => {
   return (<>
     <div className="container">
       <div className="row">
-        <div className="col-md-5">
+
+        <div className='col-md-6 signup-img'>
+          <img src="/signup.png" alt="Signup" className='img-fluid' />
+        </div>
+        <div className="col-md-6">
           <h3 className="text-center mb-4">Sign Up</h3>
           <form onSubmit={handleSubmit}>
             <div className="mb-3">
@@ -52,38 +56,11 @@ const SignupForm = () => {
             <button type="submit" className="btn btn-primary w-100">Create Account</button>
           </form>
           <p className="mt-3 text-center">
-            Already have an account? <Link href="#">Log In</Link>
+            Already have an account? 
+            <button className="cool-btn" onClick={()=>setShowLogin(!showLogin)}>Log in</button>
           </p>
         </div>
-        <div className="col-md-5">
-          <h3 className="text-center mb-4">Sign Up</h3>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">Full Name</label>
-              <input type="text" className="form-control" id="name" name="name" placeholder='Enter Name' value={formData.name} onChange={handleChange} required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="number" className="form-label">Number</label>
-              <input type="number" className="form-control" id="number" name="number" placeholder='Enter Number' value={formData.number} onChange={handleChange} required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">Email address</label>
-              <input type="email" className="form-control" id="email" name="email" placeholder='Enter Email' value={formData.email} onChange={handleChange} required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" className="form-control" id="password" name="password" placeholder='Enter Password' value={formData.password} onChange={handleChange} required />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-              <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" placeholder='Confirm Password' value={formData.confirmPassword} onChange={handleChange} required />
-            </div>
-            <button type="submit" className="btn btn-primary w-100">Create Account</button>
-          </form>
-          <p className="mt-3 text-center">
-            Already have an account? <Link href="#">Log In</Link>
-          </p>
-        </div>
+
       </div>
     </div>
   </>
--- frontend/src/Layout/AppLayout.jsx
+++ frontend/src/Layout/AppLayout.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
 import HomePage from '../Components/HomePage/HomePage'
-import SignupForm from '../Components/Login-SignUp page/SignupForm'
+
+
 // import Interview_Home_Page from "../Components/Interview page/Interview-Home-Page"
 const AppLayout = () => {
+
   return (
    <>
    <Header/>
    <HomePage/>
-   <SignupForm/>
+   {/* <SignupForm/>
+   <LoginForm/> */}
    {/* <Interview_Home_Page /> */}
    </>
   )
