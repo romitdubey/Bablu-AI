c9e13723678a647a06fafb810e066670768b9d1e
shubham7291 <sv72910501@gmail.com>
2025-04-24 14:08:38 +0530

    api chat

--- backend/core/helper.py
+++ backend/core/helper.py
@@ -2,14 +2,14 @@ import json
 
 def update_chat_history(User:bool,response):
     if User:
-        chat_messages = json.load(open("backend/user_history/chat_messages.json"))
+        chat_messages = json.load(open("../user_history/chat_messages.json"))
         chat_messages.append({"role": "user", "content": response})
-        json.dump(chat_messages, open("backend/user_history/chat_messages.json", "w"))
+        json.dump(chat_messages, open("../user_history/chat_messages.json", "w"))
         return chat_messages
     else:
-        chat_messages = json.load(open("backend/user_history/chat_messages.json"))
+        chat_messages = json.load(open("../user_history/chat_messages.json"))
         chat_messages.append({"role": "assistant", "content": response})
-        json.dump(chat_messages, open("backend/user_history/chat_messages.json", "w"))
+        json.dump(chat_messages, open("../user_history/chat_messages.json", "w"))
         return True
 
     
