b00488a4f08903231b4cfd58a876ef03716b2f75
Aryan <aryanrajput039@gmail.com>
2025-04-28 10:33:50 +0530

    minor changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCxuvCpAfSSmhd7cVS3Wzm3bnd3GoQudQ",
-    authDomain: "hackathons-5f2de.firebaseapp.com",
-    projectId: "hackathons-5f2de",
-    storageBucket: "hackathons-5f2de.firebasestorage.app",
-    messagingSenderId: "515532031820",
-    appId: "1:515532031820:web:bd9a665daae44dab0231e9",
-    measurementId: "G-MDS30FJ11V"
 };
 
 
